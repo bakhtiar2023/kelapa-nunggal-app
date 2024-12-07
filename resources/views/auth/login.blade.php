4f9f3bf644f251c747ae69534a7c758bc99e29c4
Achmad Bakhtiar <123394200+bakhtiar2023@users.noreply.github.com>
2024-12-07 15:50:00 +0700

    csrf settings

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,8 +3,7 @@
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
-        @csrf
-
+    {{ csrf_field() }}
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
